2cd366d082fc517c8a43f5461581b8992737facf
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-07-29 19:27:10 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,1 @@
--- This file was protected using skid Obfuscator v69.420 [discord.gg/makalhub]
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/93800c56180ad42c85b7efbe0c48cbba.lua"))()
